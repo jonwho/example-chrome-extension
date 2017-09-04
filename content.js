632fce89e4cee4f7b6b4ff66a42c226856a0f92b
Jon Ho <jonwho.contact@gmail.com>
2017-09-03 22:15:55 -0700

    was working with nickys golang server stuff

--- content.js
+++ content.js
@@ -12,16 +12,16 @@ var sessionToken = null;
 
 $.ajax({
   method: 'GET',
-  url: 'https://7d3139c2.ngrok.io/getSessionToken/',
+  url: 'https://447a397d.ngrok.io/getSessionToken/',
   success: function(sessionToken) {
     $.ajax({
       method: 'POST',
-      url: 'https://7d3139c2.ngrok.io/send/',
-      data: {
-        session_token: sessionToken,
+      url: 'https://447a397d.ngrok.io/send/',
+      data: JSON.stringify({
+        session_token: parseInt(sessionToken, 10),
         sender: 'chrome ext',
         message: 'foobar'
-      },
+      }),
       success: function() {
         console.log("POST send");
       }
